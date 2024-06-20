63346ff78df3bc1d609f6db75855789c6fa6a1fa
devonyou <127083854+devonyou@users.noreply.github.com>
2024-06-20 13:36:32 +0900

    [level 1] Title: 하샤드 수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,4 +1,27 @@
 function solution(x) {
-    const sum = String(x).split('').reduce((acc, v) => acc + Number(v), 0)
-    return x % sum ? false : true 
+    const arr = []
+    let num = x
+    for (let i = 0; i < String(x).length; i++) {
+        const temp = Math.floor(num % 10)
+        arr.push(temp)
+        num = num / 10
+    }
+
+    const len = arr.length
+    for (let i = 0; i < Math.floor(len / 2); i++) {
+        const temp = arr[len - 1 - i]
+        arr[len - 1 - i] = arr[i]
+        arr[i] = temp
+    }
+
+    function reduce(iter, fn, init) {
+        let acc = init
+        for (let i = 0; i < iter.length; i++) {
+            acc = fn(acc, iter[i])
+        }
+        return acc
+    }
+
+    const sum = reduce(arr, (a, b) => a + b, 0)
+    return x % sum ? false : true
 }
