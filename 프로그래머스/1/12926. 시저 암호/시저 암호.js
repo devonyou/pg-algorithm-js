79715076c1065da9803579fb9a63595c11abc7b4
devonyou <127083854+devonyou@users.noreply.github.com>
2024-06-21 13:01:18 +0900

    [level 1] Title: 시저 암호, Time: 29.52 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,21 +1,16 @@
 function solution(s, n) {
     const start = 'a'.charCodeAt(0)
     const end = 'z'.charCodeAt(0)
-    let shift = n % (end - start + 1)
+    const count = end - start + 1
+    const shift = n % count
     if(shift === 0) return s
     
     const r = [...s].map((v, i)=>{
         if(v === ' ') return ' '
         const is_upper = v.toUpperCase() === v
         const code = v.toLowerCase().charCodeAt(0)
-        let char_code = 0
-        if(code + shift > end){
-            char_code = start + (((code + shift) % end) - 1)
-        } else {
-            char_code = code + shift
-        }
+        const char_code = ((code + shift - start) % count) + start
         const alpha = String.fromCharCode(char_code)
-        
         return is_upper ? alpha.toUpperCase() : alpha.toLowerCase()
     })
     
