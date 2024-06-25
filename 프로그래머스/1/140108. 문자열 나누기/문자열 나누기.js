247e85c451faf305454995f95423095affec5faf
devonyou <127083854+devonyou@users.noreply.github.com>
2024-06-26 00:06:22 +0900

    [level 1] Title: 문자열 나누기, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,24 +1,17 @@
 function solution(s) {
-    function splitChar(str) {
-        if (str.length === 1) return str
-        let first_count = 1
-        let other_count = 0
-        let i = 1
-        while (first_count !== other_count) {
-            if ([...str][0] === [...str][i]) first_count++
-            else other_count++
-            i++
-        }
-        return str.slice(0, i)
-    }
+    let counter = 0
+    let ch = ''
+    let cnt = 0
 
-    let i = 0
-    let str = s
-    while (str) {
-        const split_char = splitChar(str)
-        str = str.replace(split_char, '')
-        i++
+    for (let i = 0; i < s.length; i++) {
+        if (cnt === 0) {
+            counter++
+            ch = s[i]
+            cnt++
+        } else {
+            if (ch !== s[i]) cnt--
+            else cnt++
+        }
     }
-    
-    return i
+    return counter
 }
